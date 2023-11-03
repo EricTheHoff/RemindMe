c2837ef30f4ee5f6c61c45af0df8b8e5a5b2f0f9
Eric Hoffman <eric.peter.hoffman@gmail.com>
2023-11-03 15:57:01 -0600

    Edit/Delete buttons are rendered now.

--- src/components/ModeButtons.jsx
+++ src/components/ModeButtons.jsx
@@ -1,8 +1,16 @@
 
 
-const ModeButtons = () => {
-  return (
-    <div>ModeButtons</div>
+const ModeButtons = ({ isEditing, changeMode, id }) => {
+
+  return isEditing ? (
+    <td>
+        <button onClick={changeMode}>Save</button>
+    </td>
+  ) : (
+    <td>
+        <button onClick={changeMode}>Edit</button>
+        <button>Delete</button>
+    </td>
   )
 }
 
